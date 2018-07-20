d80c5edfbb2adbefb3b5eac67a6c5c165fb9f178
canyuva <canyuva06@gmail.com>
2018-07-20 10:18:29 +0000

    Lombok is now used in DTO Classes

--- src/com/myTeam/shared/CategoryDTO.java
+++ src/com/myTeam/shared/CategoryDTO.java
@@ -1,25 +1,12 @@
 package com.myTeam.shared;
 
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class CategoryDTO implements Serializable {
-    private int pk_cat_id;
-    private String cat_name;
-
-    public int getPk_cat_id() {
-        return pk_cat_id;
-    }
-
-    public void setPk_cat_id(int pk_cat_id) {
-        this.pk_cat_id = pk_cat_id;
-    }
-
-    public String getCat_name() {
-        return cat_name;
-    }
-
-    public void setCat_name(String cat_name) {
-        this.cat_name = cat_name;
-    }
+   private int pk_cat_id;
+   private String cat_name;
 }
--- src/com/myTeam/shared/UserDTO.java
+++ src/com/myTeam/shared/UserDTO.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 import java.io.Serializable;
 
-
+@Data
 public class UserDTO implements Serializable {
     private int pk_user_id;
 
@@ -17,52 +17,4 @@ public class UserDTO implements Serializable {
     private String gender;
 
     private int fk_team_id;
-
-    public int getPk_user_id() {
-        return pk_user_id;
-    }
-
-    public void setPk_user_id(int pk_user_id) {
-        this.pk_user_id = pk_user_id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public int getFk_team_id() {
-        return fk_team_id;
-    }
-
-    public void setFk_team_id(int fk_team_id) {
-        this.fk_team_id = fk_team_id;
-    }
 }
